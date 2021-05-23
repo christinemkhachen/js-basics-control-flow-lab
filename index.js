e4dcf54c5fe5961ccbb95f3227d625a5bc9cbe43
christinemkhachen <christinemkhachen@gmail.com>
2021-05-23 17:29:14 -0400

    commit tree de09c9289810b6a6f389ef7dcba36d5d6099a061

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+      return "This one is on me!";
+  } 
+
+  if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+
+  if (feet > 2500)      {
+    return "No can do."
+  }  
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  // This is a ternary; ternary is shorthand for if statement
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+// this is the same as: this is never going to run because we havent called or invoked the funtion. this is purely an example of a conditional rather than a ternary.
+function conditionalCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default: 
+      return "Bye." 
+  }
+}
+function equivalentToSwitch(tip) {
+  if (tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye"
+  }
 }
